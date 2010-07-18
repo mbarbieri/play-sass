6d914cd9f8b626ca9359a0f70bac96411db60c92
Johnny Gozde <johnny@jgoz.net>
2010-07-19 02:29:42 +0800

    Added file extension support for both .sass and .scss

--- src/play/modules/sass/Plugin.java
+++ src/play/modules/sass/Plugin.java
@@ -1,6 +1,7 @@
 package play.modules.sass;
 
 import java.io.PrintStream;
+
 import play.Play;
 import play.PlayPlugin;
 import play.mvc.Http.Request;
@@ -9,7 +10,7 @@ import play.vfs.VirtualFile;
 
 public class Plugin extends PlayPlugin {
     Engine sass;
-    
+
     @Override
     public void onLoad() {
         sass = new Engine(Play.getVirtualFile("haml-3.0.13").getRealFile());
@@ -17,18 +18,18 @@ public class Plugin extends PlayPlugin {
 
     @Override
     public boolean serveStatic(VirtualFile file, Request request, Response response) {
-        //FIXME remove : reset engine to recompile at each css
-        
-        if(file.getName().endsWith(".scss")) {
+        // FIXME remove : reset engine to recompile at each css
+
+        if (isSass(file) || isScss(file)) {
             try {
                 String css = sass.compile(file.getRealFile(), Play.mode == Play.Mode.DEV);
                 response.contentType = "text/css";
                 response.status = 200;
-                if(Play.mode == Play.Mode.PROD) {
+                if (Play.mode == Play.Mode.PROD) {
                     response.cacheFor("1h");
                 }
                 response.print(css);
-            } catch(Exception e) {
+            } catch (Exception e) {
                 response.contentType = "text/css";
                 response.status = 500;
                 response.print("Oops,\n");
@@ -38,12 +39,28 @@ public class Plugin extends PlayPlugin {
         }
 
         // Discard ruby sources
-        if(file.getName().endsWith(".rb")) {
+        if (isRuby(file)) {
             response.status = 404;
             return true;
         }
 
         return super.serveStatic(file, request, response);
+    }
+
+    private static boolean isSass(VirtualFile file) {
+        return fileExtensionIs(file, ".sass");
+    }
+
+    private static boolean isScss(VirtualFile file) {
+        return fileExtensionIs(file, ".scss");
+    }
+
+    private static boolean isRuby(VirtualFile file) {
+        return fileExtensionIs(file, ".rb");
+    }
+
+    private static boolean fileExtensionIs(VirtualFile file, String ext) {
+        return file.getName().endsWith(ext);
     }
 
 }
